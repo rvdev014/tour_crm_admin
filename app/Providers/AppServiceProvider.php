a1889096cb3a431459f7206bdb4fc6ee0dba3d07
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2024-12-23 21:15:37 +0500

    prepare production optimized docker images

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+        if (env('APP_SCHEME') === 'https') {
             URL::forceScheme('https');
         }
 
