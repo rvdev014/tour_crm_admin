e8242f7bcb47a6ccd061fd1538babed9e5abd4f9
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2025-01-23 08:33:13 +0500

    Complete 3 sprint

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('export/{tour}', [\App\Http\Controllers\ExportController::class, 'export'])->name('export');
+Route::get('export-client/{tour}', [\App\Http\Controllers\ExportController::class, 'exportClient'])->name('export');
