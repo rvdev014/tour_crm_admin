74213b7bdb54aa5d1070395c997ea0991b4b7872
Ravshan Davlatov <r.davlatov@ssd.uz>
2025-02-06 14:22:24 +0500

    Add UserResource email unique validation

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -36,6 +36,7 @@ class UserResource extends Resource
                 Forms\Components\TextInput::make('email')
                     ->email()
                     ->required()
+                    ->unique(ignoreRecord: true)
                     ->maxLength(255),
                 //                Forms\Components\DateTimePicker::make('email_verified_at'),
                 Forms\Components\TextInput::make('password')
