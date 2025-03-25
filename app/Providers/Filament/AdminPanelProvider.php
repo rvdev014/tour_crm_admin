7e3e5ff0c5766636c0090722585b4c6cc0045cee
Ravshan (office) <ravshandavlatov014@gmail.com>
2025-03-25 12:46:11 +0500

    Change nginx to volume

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -56,7 +56,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->viteTheme('resources/css/filament/admin/theme.css')
+//            ->viteTheme('resources/css/filament/admin/theme.css')
             /*->plugins([
                 StickyHeaderPlugin::make(),
             ])*/;
